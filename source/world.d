75aa1d06ec4e9cf30a5a7d0aefdae687ecb55e06
minecraft8997 <35162137+minecraft8997@users.noreply.github.com>
2023-07-01 01:26:02 +0300

    (MIGHT NOT COMPILE) Things

--- source/world.d
+++ source/world.d
@@ -163,9 +163,9 @@ class World {
 		blocks = CreateBlockArray();
 		debug writef("Got an array, size: %d\n", blocks.length);
 
-		for (short y = 0; y < size.y; ++y) {
-			for (short z = 0; z < size.z; ++z) {
-				for (short x = 0; x < size.x; ++x) {
+		for (ushort y = 0; y < size.y; ++y) {
+			for (ushort z = 0; z < size.z; ++z) {
+				for (ushort x = 0; x < size.x; ++x) {
 					ubyte type;
 					if (y > size.y / 2) {
 						type = Block.Air;
